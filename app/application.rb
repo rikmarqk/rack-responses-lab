0b2336df77f051c694107ecd63a7fab279485b59
rikmarqk <rikmarqk@gmail.com>
2020-10-28 10:34:54 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+
+        hour = Time.now.to_s.split(' ')[1].split(':')[0].to_i
+        if hour < 12
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+
+            resp.finish
+    end
+
+end
